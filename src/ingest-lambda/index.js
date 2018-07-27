fff7c1ed0d96af2a0ac72fa417b9bef35c66b661
Ryan Gross <ryan.gross@parivedasolutions.com>
2018-07-27 10:44:51 -0500

    Fixing intermittent errors in ingest lambda

--- src/ingest-lambda/index.js
+++ src/ingest-lambda/index.js
@@ -3,7 +3,6 @@ var async = require('async');
 var awsHelpers = require('aws-helpers');
 var crypto = require('crypto');
 var PriceHistoryBatch = require('price-history-batch');
-var instanceTypes = ['d2.2xlarge', 'm3.large', 'c3.xlarge', 'c3.2xlarge', 'c5.9xlarge', 'i3.2xlarge', 'r3.4xlarge', 'r4.16xlarge', 't2.large', 'm3.2xlarge', 'r4.large', 'c5.18xlarge', 'r3.2xlarge', 'r4.4xlarge', 'c3.large', 'c4.8xlarge', 't2.2xlarge', 'm4.16xlarge', 'r4.8xlarge', 'p2.xlarge', 'i3.xlarge', 'c3.8xlarge', 'c4.xlarge', 't2.micro', 'c3.4xlarge', 'm4.large', 'c5.4xlarge', 't2.medium', 'c5.large', 'i2.8xlarge', 'd2.4xlarge', 't2.xlarge', 'd2.8xlarge', 'c4.4xlarge', 'r4.2xlarge', 'i3.16xlarge', 'x1.32xlarge', 'c5.2xlarge', 'm4.xlarge', 'd2.xlarge', 'c4.2xlarge', 'c5.xlarge', 't2.small', 'p2.16xlarge', 'r4.xlarge', 'm4.10xlarge', 'r3.large', 'i2.xlarge', 'm3.medium', 'i2.4xlarge', 'r3.xlarge', 'c4.large', 'p2.8xlarge', 'm4.2xlarge', 'i2.2xlarge', 'x1.16xlarge', 'i3.large', 'r3.8xlarge', 'i3.4xlarge', 'm4.4xlarge', 'i3.8xlarge', 'm3.xlarge'];
 
 exports.handler = (event, context, callback) => {
 	console.log('Ingest handler received event:', event);
@@ -59,7 +58,68 @@ function split(array, size){
 }
 
 function queryPriceHistory(startTime, endTime, callback){
-	console.log(instanceTypes);
+	var instanceTypes = ["d2.2xlarge",
+"m3.large",
+"c3.xlarge",
+"c3.2xlarge",
+"c5.9xlarge",
+"i3.2xlarge",
+"r3.4xlarge",
+"r4.16xlarge",
+"t2.large",
+"m3.2xlarge",
+"r4.large",
+"c5.18xlarge",
+"r3.2xlarge",
+"r4.4xlarge",
+"c3.large",
+"c4.8xlarge",
+"t2.2xlarge",
+"m4.16xlarge",
+"r4.8xlarge",
+"p2.xlarge",
+"i3.xlarge",
+"c3.8xlarge",
+"c4.xlarge",
+"t2.micro",
+"c3.4xlarge",
+"m4.large",
+"c5.4xlarge",
+"t2.medium",
+"c5.large",
+"i2.8xlarge",
+"d2.4xlarge",
+"t2.xlarge",
+"d2.8xlarge",
+"c4.4xlarge",
+"r4.2xlarge",
+"i3.16xlarge",
+"x1.32xlarge",
+"c5.2xlarge",
+"m4.xlarge",
+"d2.xlarge",
+"c4.2xlarge",
+"c5.xlarge",
+"t2.small",
+"p2.16xlarge",
+"r4.xlarge",
+"m4.10xlarge",
+"r3.large",
+"i2.xlarge",
+"m3.medium",
+"i2.4xlarge",
+"r3.xlarge",
+"c4.large",
+"p2.8xlarge",
+"m4.2xlarge",
+"i2.2xlarge",
+"x1.16xlarge",
+"i3.large",
+"r3.8xlarge",
+"i3.4xlarge",
+"m4.4xlarge",
+"i3.8xlarge",
+"m3.xlarge"];
 	var instanceTypeBatches = split(instanceTypes, 4);
 	var ec2 = new AWS.EC2({region: awsRegion});
 	var awsRegion = process.env.AWS_REGION;
@@ -94,11 +154,14 @@ function queryPriceHistory(startTime, endTime, callback){
 				console.log(data);
 				var batchRecord = new PriceHistoryBatch(startTime.toISOString(), endTime.toISOString(), data.SpotPriceHistory);
 				var partitionKey = crypto.createHash('md5').update(new Date().toISOString()).digest('hex');
-				awsHelpers.sendDataToKinesis(awsRegion, outputStream, partitionKey, JSON.stringify(batchRecord), callback);
-				if(data.NextToken) {
-					console.log("Getting the next batch of results for instance type batch ", instanceTypeBatch)
-					getSpotPrices(instanceTypeBatch, callback, data.NextToken);
-				}
+				awsHelpers.sendDataToKinesis(awsRegion, outputStream, partitionKey, JSON.stringify(batchRecord), function(err, result) {
+					if(data.NextToken) {
+						console.log("Getting the next batch of results for instance type batch ", instanceTypeBatch)
+						getSpotPrices(instanceTypeBatch, callback, data.NextToken);
+					} else {
+						callback(err, result);
+					}
+				});
 			}
 		});
 	}
