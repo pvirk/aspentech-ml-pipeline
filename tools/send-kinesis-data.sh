7d2bd1240de82fa50cf8a5b6c749e9626627044e
Vinod Shukla <vsnyc@users.noreply.github.com>
2018-07-28 03:46:14 -0400

    Deleting files no longer necessary

--- tools/send-kinesis-data.sh
+++ tools/send-kinesis-data.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+#Expected input: BatchRequestStream-<StackSuffix>
 streamName=$1
 aws kinesis put-record --stream-name $streamName --data file://tools/kinesisData.json --partition-key "key"
